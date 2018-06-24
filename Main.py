966e290e5cf395a61fe8bd4a6e9e40c3d0d0bbc4
far-galaxy <40544219+far-galaxy@users.noreply.github.com>
2018-06-24 19:40:25 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -14,7 +14,7 @@ offswicth = interfaces.Pin(5,False)
 parashute = interfaces.Pin(10,True)
 parashute.set_pin(False)
 
-#sim808 = sensors.SIM808()
+sim808 = sensors.SIM808()
 phone = "+78005553535"
 nrf0 = interfaces.NRF24L01(0)
 
@@ -122,7 +122,7 @@ if status == 2:
 	
 	temp = sensors.getP_Tv()
     p = temp[0]*256+temp[1]
-	h = 18400*(1,0733)*(log10(p0/p))
+	h = 18400*(1.0733)*(log10(p0/p))
 	
 	time_ff = Time_of_FF(h)
 	text = "Apogee: " + h + " m. The maximum estimated time of free fall: " + time_ff + " seconds. "
